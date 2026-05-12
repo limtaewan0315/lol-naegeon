70dcedb47a3d5f20549a3038ba26c2d61c7ce389
limtaewan0315 <limtaesan0315@gmail.com>
2026-05-12 17:21:19 +0900

    Update page.tsx

--- lol-naegeon/src/app/page.tsx
+++ lol-naegeon/src/app/page.tsx
@@ -1885,7 +1885,7 @@ export default function Home() {
           style={{
             height: '90vh', maxHeight: 750, objectFit: 'cover', objectPosition: '70% top',
             opacity: 0.5,
-            transform: 'translateX(35%)',
+            transform: 'translateX(30%)',
             WebkitMaskImage: 'linear-gradient(to left, transparent 0%, black 55%, black 100%), linear-gradient(to top, transparent 0%, black 20%)',
             WebkitMaskComposite: 'destination-in',
             maskImage: 'linear-gradient(to left, transparent 0%, black 55%, black 100%), linear-gradient(to top, transparent 0%, black 20%)',
